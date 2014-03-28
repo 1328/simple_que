66e263b4c08f41c7e98e956fb4e9d46b6fb0a5e9
1328 <jc1328@gmail.com>
2014-03-28 15:21:33 -0400

    Cleaned up code, including changing to use camel case and other suggestions by CondosInQueens, and also added a bit of error checking.

--- scripts/run.py
+++ scripts/run.py
@@ -4,7 +4,7 @@ import os.path
 BASE_PATH = os.path.normpath('c:/tmp')
 
 def main():
-    q = simple_que.File_Que(BASE_PATH)
+    q = simple_que.FileQue(BASE_PATH)
     q.add_job('just added a new job')
     for j in q:
         print('{}:{}'.format(j.fn, j.cmd))
@@ -14,9 +14,15 @@ def main():
         else:
             print('\tlocking {}'.format(j.fn))
             q.lock(j)
-    if not q.is_locked(j):
-        print('deleting job {}'.format(j.fn))
+    print('deleting job {}'.format(j.fn))
+    if q.is_locked(j):
+        q.unlock(j)
+    try:
         q.delete_job(j)
+        q.lock(j)
+    except simple_que.FileQueException as e:
+        print(e)
+
 
 
 if __name__ == '__main__':
